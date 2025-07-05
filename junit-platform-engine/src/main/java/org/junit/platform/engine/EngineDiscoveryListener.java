006b70f0c70f54ac216cc8fe07d507b9e3b3c955
Sahil Agarwal <sahil11875@gmail.com>
2025-07-05 16:45:09 +0530

    Updated

--- junit-platform-engine/src/main/java/org/junit/platform/engine/EngineDiscoveryListener.java
+++ junit-platform-engine/src/main/java/org/junit/platform/engine/EngineDiscoveryListener.java
@@ -52,7 +52,7 @@ public interface EngineDiscoveryListener {
 	 * @see SelectorResolutionResult
 	 */
 	default void selectorProcessed(UniqueId engineId, DiscoverySelector selector, SelectorResolutionResult result) {
-		System.out.println("Test review comment 26");
+		System.out.println("Test review comment 27");
 	}
 
 	/**
